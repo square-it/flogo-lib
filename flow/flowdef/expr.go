061285fbd4df6074d15dd574763ca805b4c7ae3d
Frank Martinez <fmartinez@tibco.com>
2016-09-24 00:52:02 -0400

    fix link expression evaluation under error handler task

--- flow/flowdef/expr.go
+++ flow/flowdef/expr.go
@@ -3,22 +3,24 @@ package flowdef
 import "github.com/TIBCOSoftware/flogo-lib/core/data"
 
 // LinkExprManager interface that defines a Link Expression Manager
-//todo rename
 type LinkExprManager interface {
 
 	// EvalLinkExpr evaluate the link expression
 	EvalLinkExpr(link *Link, scope data.Scope) bool
 }
 
-// GetExpressionLinks get the links of the definition that are of type LtExpression
+// GetExpressionLinks gets the links of the definition that are of type LtExpression
 func GetExpressionLinks(def *Definition) []*Link {
 
 	var links []*Link
+
 	getExpressionLinks(def.RootTask(), &links)
+	getExpressionLinks(def.ErrorHandlerTask(), &links)
 
 	return links
 }
 
+// getExpressionLinks gets the links under the specified task that are of type LtExpression
 func getExpressionLinks(task *Task, links *[]*Link) {
 
 	for _, link := range task.ChildLinks() {
