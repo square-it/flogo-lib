a23f77e184452acc105211a768c40f7a1eb2bd15
vnalawad <vnalawad@tibco.com>
2017-02-08 18:11:38 -0800

    Wrapper for logrus

--- logger/logger.go
+++ logger/logger.go
@@ -1,27 +1,28 @@
 package logger
 
 import (
-	"github.com/goburrow/gol"
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	"strings"
 )
 
 type Logger interface {
-	Trace(string)
-	TraceEnabled() bool
-	Debug(string)
+	Debug(...interface{})
 	DebugEnabled() bool
-	Info(string)
+	Info(...interface{})
 	InfoEnabled() bool
-	Warn(string)
+	Warn(...interface{})
 	WarnEnabled() bool
-	Error(string)
+	Error(...interface{})
 	ErrorEnabled() bool
 	SetLogLevel(Level)
 }
 type Level int
 
+var loggerMap = make(map[string]interface{})
+
 const (
-	Trace Level = iota
-	Debug
+	Debug Level = iota
 	Info
 	Warn
 	Error
@@ -29,80 +30,112 @@ const (
 
 type FlogoLogger struct {
 	loggerName string
-	loggerImpl gol.Logger
+	loggerImpl *logrus.Logger
 }
 
-func (logger *FlogoLogger) Trace(message string) {
-	logger.loggerImpl.Tracef(message)
+const ()
+
+func init() {
+
 }
 
-// TraceEnabled checks if Trace level is enabled.
-func (logger *FlogoLogger) TraceEnabled() bool {
-	return logger.loggerImpl.TraceEnabled()
+type FlogoFormatter struct {
+	loggerName string
+}
+
+func (f *FlogoFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+
+	logEntry := fmt.Sprintf("%s %-6s [%s] - %s\n", entry.Time.Format("2006-01-02 15:04:05.000000"), getLevel(entry.Level), f.loggerName, strings.TrimPrefix(strings.TrimSuffix(entry.Message, "]"), "["))
+	return []byte(logEntry), nil
+}
+
+func getLevel(level logrus.Level) string {
+	switch level {
+	case logrus.DebugLevel:
+		return "DEBUG"
+	case logrus.InfoLevel:
+		return "INFO"
+	case logrus.ErrorLevel:
+		return "ERROR"
+	case logrus.WarnLevel:
+		return "WARN"
+	case logrus.PanicLevel:
+		return "PANIC"
+	case logrus.FatalLevel:
+		return "FATAL"
+	}
+
+	return "UNKNOWN"
 }
 
 // Debugf logs message at Debug level.
-func (logger *FlogoLogger) Debug(message string) {
-	logger.loggerImpl.Debugf(message)
+func (logger *FlogoLogger) Debug(args ...interface{}) {
+	logger.loggerImpl.Debug(args)
 }
 
 // DebugEnabled checks if Debug level is enabled.
 func (logger *FlogoLogger) DebugEnabled() bool {
-	return logger.loggerImpl.DebugEnabled()
+	return logger.loggerImpl.Level >= logrus.DebugLevel
 }
 
 // Infof logs message at Info level.
-func (logger *FlogoLogger) Info(message string) {
-	logger.loggerImpl.Infof(message)
+func (logger *FlogoLogger) Info(args ...interface{}) {
+	logger.loggerImpl.Info(args)
 }
 
 // InfoEnabled checks if Info level is enabled.
 func (logger *FlogoLogger) InfoEnabled() bool {
-	return logger.loggerImpl.InfoEnabled()
+	return logger.loggerImpl.Level >= logrus.InfoLevel
 }
 
 // Warnf logs message at Warning level.
-func (logger *FlogoLogger) Warn(message string) {
-	logger.loggerImpl.Warnf(message)
+func (logger *FlogoLogger) Warn(args ...interface{}) {
+	logger.loggerImpl.Warn(args)
 }
 
 // WarnEnabled checks if Warning level is enabled.
 func (logger *FlogoLogger) WarnEnabled() bool {
-	return logger.loggerImpl.WarnEnabled()
+	return logger.loggerImpl.Level >= logrus.WarnLevel
 }
 
 // Errorf logs message at Error level.
-func (logger *FlogoLogger) Error(message string) {
-	logger.loggerImpl.Errorf(message)
+func (logger *FlogoLogger) Error(args ...interface{}) {
+	logger.loggerImpl.Error(args)
 }
 
 // ErrorEnabled checks if Error level is enabled.
 func (logger *FlogoLogger) ErrorEnabled() bool {
-	return logger.loggerImpl.ErrorEnabled()
+	return logger.loggerImpl.Level >= logrus.ErrorLevel
 }
 
 //SetLog Level
 func (logger *FlogoLogger) SetLogLevel(logLevel Level) {
 	switch logLevel {
-	case Trace:
-		logger.loggerImpl.(*gol.DefaultLogger).SetLevel(gol.Trace)
 	case Debug:
-		logger.loggerImpl.(*gol.DefaultLogger).SetLevel(gol.Debug)
+		logger.loggerImpl.Level = logrus.DebugLevel
 	case Info:
-		logger.loggerImpl.(*gol.DefaultLogger).SetLevel(gol.Info)
+		logger.loggerImpl.Level = logrus.InfoLevel
 	case Error:
-		logger.loggerImpl.(*gol.DefaultLogger).SetLevel(gol.Error)
+		logger.loggerImpl.Level = logrus.ErrorLevel
 	case Warn:
-		logger.loggerImpl.(*gol.DefaultLogger).SetLevel(gol.Warn)
+		logger.loggerImpl.Level = logrus.WarnLevel
 	default:
-		logger.loggerImpl.(*gol.DefaultLogger).SetLevel(gol.Error)
+		logger.loggerImpl.Level = logrus.ErrorLevel
 	}
 }
 
 func GetLogger(name string) Logger {
-	logger := gol.GetLogger(name)
-	return &FlogoLogger{
-		loggerName: name,
-		loggerImpl: logger,
+	logger := loggerMap[name]
+	if logger == nil {
+		logImpl := logrus.New()
+		logImpl.Formatter = &FlogoFormatter{
+			loggerName: name,
+		}
+		logger = &FlogoLogger{
+			loggerName: name,
+			loggerImpl: logImpl,
+		}
+		loggerMap[name] = logger
 	}
+	return logger.(Logger)
 }
