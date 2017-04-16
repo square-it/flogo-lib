eadc0fbb224dd76451c61832fc962aeadbe86d5f
Frank Martinez <fmartinez@tibco.com>
2017-04-16 12:45:45 -0400

    Switch settings to map[string]interface{}, add helper GetSettings function for string settings

--- core/trigger/config.go
+++ core/trigger/config.go
@@ -2,31 +2,30 @@ package trigger
 
 // Config is the configuration for a Trigger
 type Config struct {
-	Name     string            `json:"name"`
-	Id       string            `json:"id"`
-	Ref      string            `json:"ref"`
-	Settings map[string]string `json:"settings"`
-	Handlers []*HandlerConfig  `json:"handlers"`
+	Name     string                 `json:"name"`
+	Id       string                 `json:"id"`
+	Ref      string                 `json:"ref"`
+	Settings map[string]interface{} `json:"settings"`
+	Handlers []*HandlerConfig       `json:"handlers"`
 
 	//deprecated
 	//Settings map[string]string `json:"settings"`
 	Endpoints []*EndpointConfig `json:"endpoints"`
 }
 
-// HandlerConfig is the configuration for the Trigger Handler
-type HandlerConfig struct {
-	ActionId string            `json:"actionId"`
-	Settings map[string]string `json:"settings"`
+func (c *Config) GetSetting(setting string) string {
+	return c.Settings[setting].(string)
 }
 
+// HandlerConfig is the configuration for the Trigger Handler
+type HandlerConfig struct {
+	ActionId string                 `json:"actionId"`
+	Settings map[string]interface{} `json:"settings"`
+}
 
-//// Trigger is the configuration for the Trigger
-//type TriggerConfig struct {
-//	Id       string                 `json:"id"`
-//	Ref      string                 `json:"ref"`
-//	Settings map[string]interface{} `json:"settings"`
-//	Handlers []*TriggerHandler      `json:"handlers"`
-//}
+func (hc *HandlerConfig) GetSetting(setting string) string {
+	return hc.Settings[setting].(string)
+}
 
 // EndpointConfig is the configuration for a specific endpoint for the
 // Trigger // Deprecated
@@ -36,4 +35,3 @@ type EndpointConfig struct {
 	ActionURI  string            `json:"actionURI"`
 	Settings   map[string]string `json:"settings"`
 }
-
