7e6e946928a3799b2b28143cfbf8f17762cb2624
vnalawad <vnalawad@tibco.com>
2017-05-04 11:46:00 -0700

    Added support for error code

--- core/activity/error.go
+++ core/activity/error.go
@@ -3,6 +3,7 @@ package activity
 // Error is an activity error
 type Error struct {
 	errorStr  string
+	errorCode int
 	errorData interface{}
 }
 
@@ -16,6 +17,10 @@ func NewErrorWithData(errorText string, errorData interface{}) *Error {
 	return &Error{errorStr: errorText, errorData: errorData}
 }
 
+func NewErrorWithDataAndCode(errorText string, errorData interface{}, code int) *Error {
+	return &Error{errorStr: errorText, errorData: errorData, errorCode: code}
+}
+
 // Error implements error.Error()
 func (e *Error) Error() string {
 	return e.errorStr
@@ -24,4 +29,9 @@ func (e *Error) Error() string {
 // Data returns any associated error data
 func (e *Error) Data() interface{} {
 	return e.errorData
+}
+
+// Code returns any associated error code
+func (e *Error) Code() int {
+	return e.errorCode
 }
