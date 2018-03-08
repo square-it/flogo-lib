fb4679306595dc3cc8264e688eebde72b29d717f
Tracy Li <tibcotracy@gmail.com>
2018-03-08 08:06:37 -0600

    Return int not int64

--- core/mapper/exprmapper/function/number/random/random.go
+++ core/mapper/exprmapper/function/number/random/random.go
@@ -1,10 +1,10 @@
 package random
 
 import (
-	"encoding/json"
 	"math/rand"
-	"strconv"
 	"time"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/data"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/mapper/exprmapper/expression/function"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
@@ -27,36 +27,13 @@ func (s *Random) GetCategory() string {
 	return "number"
 }
 
-func (s *Random) Eval(limitIn interface{}) int64 {
-	limit, err := ConvertToInt64(limitIn)
+func (s *Random) Eval(limitIn interface{}) int {
+	limit, err := data.CoerceToInteger(limitIn)
 	if err != nil {
 		log.Errorf("Convert %+v to int error %s", limitIn, err.Error())
 		limit = 10
 	}
 	log.Debugf("Generate sudo-random integer number within the scope of [0, %d)", limit)
 	rand.Seed(time.Now().UnixNano())
-	return rand.Int63n(limit)
-}
-
-func ConvertToInt64(value interface{}) (int64, error) {
-	switch t := value.(type) {
-	case string:
-		return strconv.ParseInt(t, 10, 64)
-	case int:
-		return int64(t), nil
-	case int64:
-		return t, nil
-	case float64:
-		return int64(t), nil
-	default:
-		v, err := json.Marshal(value)
-		if err != nil {
-			return 0, err
-		}
-		i, err := strconv.ParseInt(string(v), 10, 64)
-		if err != nil {
-			return 0, err
-		}
-		return i, nil
-	}
+	return rand.Intn(limit)
 }
