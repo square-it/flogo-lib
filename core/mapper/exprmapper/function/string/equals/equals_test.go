3ef4a178cde9d84d33792ac921e22503d3031065
Tracy Li <tibcotracy@gmail.com>
2018-03-28 18:28:07 -0500

    Remove ignore case argument for string.equals

--- core/mapper/exprmapper/function/string/equals/equals_test.go
+++ core/mapper/exprmapper/function/string/equals/equals_test.go
@@ -11,28 +11,18 @@ import (
 var s = &Equals{}
 
 func TestStaticFunc_Starts_with(t *testing.T) {
-	final1 := s.Eval("TIBCO FLOGO", "TIBCO", true)
+	final1 := s.Eval("TIBCO FLOGO", "TIBCO")
 	fmt.Println(final1)
 	assert.Equal(t, false, final1)
 
-	final2 := s.Eval("TIBCO", "tibco", true)
+	final2 := s.Eval("TIBCO", "tibco")
 	fmt.Println(final2)
-	assert.Equal(t, true, final2)
+	assert.Equal(t, false, final2)
 
 }
 
 func TestExpression(t *testing.T) {
-	fun, err := expression.NewFunctionExpression(`string.equals("TIBCO FLOGO", "TIBCO FLOGO", false)`).GetFunction()
-	assert.Nil(t, err)
-	assert.NotNil(t, fun)
-	v, err := fun.Eval()
-	assert.Nil(t, err)
-	assert.NotNil(t, v)
-	assert.Equal(t, true, v[0])
-}
-
-func TestExpressionIgnoreCase(t *testing.T) {
-	fun, err := expression.NewFunctionExpression(`string.equals("TIBCO flogo", "TIBCO FLOGO", true)`).GetFunction()
+	fun, err := expression.NewFunctionExpression(`string.equals("TIBCO FLOGO", "TIBCO FLOGO")`).GetFunction()
 	assert.Nil(t, err)
 	assert.NotNil(t, fun)
 	v, err := fun.Eval()
