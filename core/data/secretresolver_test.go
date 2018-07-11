0b50342910668f4fdfeeb9462e59e245036ccfcd
Vijay Nalawade <vnalawad@tibco.com>
2018-07-10 17:08:15 -0700

    Minor refactoring

--- core/data/secretresolver_test.go
+++ core/data/secretresolver_test.go
@@ -8,21 +8,37 @@ import (
 	"github.com/TIBCOSoftware/flogo-lib/config"
 )
 
+func TestSecretKeyNotSet(t *testing.T) {
+	defer func() {
+		SetSecretValueDecoder(nil)
+	}()
+	decoder := GetSecretValueDecoder()
+	decrypted, err := decoder.DecodeValue("mysecurepassword3")
+	assert.Nil(t, err)
+	assert.Equal(t, "mysecurepassword3", decrypted)
+	SetSecretValueDecoder(nil)
+}
+
 func TestSecretKeyEnv(t *testing.T) {
 	os.Setenv(config.ENV_SECRET_KEY_KEY, "mysecretkey1")
-	defer os.Unsetenv(config.ENV_SECRET_KEY_KEY)
-
-	decoder := GetSecretValueDecoder()
-	decrypted, err := decoder.DecodeValue("Dpn7oxbWZPHlLkPkzgm+qZFHfGHlAKoFXcu5RhbNlQZS")
+	defer func() {
+		os.Unsetenv(config.ENV_SECRET_KEY_KEY)
+		SetSecretValueDecoder(nil)
+	}()
+	decrypted, err := GetSecretValueDecoder().DecodeValue("Dpn7oxbWZPHlLkPkzgm+qZFHfGHlAKoFXcu5RhbNlQZS")
 	assert.Nil(t, err)
 	assert.Equal(t, "mysecurepassword1", decrypted)
 }
 
 func TestSecretKey(t *testing.T) {
-	decoder := &KeyBasedSecretValueDecoder{Key: "mysecretkey2"}
-	SetSecretValueDecoder(decoder)
-	decrypted, err := decoder.DecodeValue("4hmxSs2mjKfExP0lLLW9R9hK8631ce2r5RWDKYJjDZV2")
+	defer func() {
+		SetSecretValueDecoder(nil)
+	}()
+	SetSecretValueDecoder(&KeyBasedSecretValueDecoder{Key: "mysecretkey2"})
+	decrypted, err := GetSecretValueDecoder().DecodeValue("4hmxSs2mjKfExP0lLLW9R9hK8631ce2r5RWDKYJjDZV2")
 	assert.Nil(t, err)
 	assert.Equal(t, "mysecurepassword2", decrypted)
 }
+
+
 
